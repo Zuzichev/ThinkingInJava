12d5b2ed48b83ade68629f86326631733443f500
Zuzichev <zuzichev@mail.ru>
2021-06-18 20:18:26 +0500

    Initial commit

---
+++ src/chapter2page93/exercise1.java
@@ -0,0 +1,11 @@
+package chapter2page93;
+
+public class exercise1 {
+
+    public static void main(String[] args) {
+        int intDefaultOut;
+        char charDefaultOut;
+        System.out.println(intDefaultOut);
+        System.out.println(charDefaultOut);
+    }
+}
