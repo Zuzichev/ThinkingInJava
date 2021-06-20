2a94d8a414f02c7415d02af9be2cacc4df162bb0
Zuzichev <zuzichev@mail.ru>
2021-06-20 14:47:07 +0500

    init

---
+++ src/chapter2page93/exercise5.java
@@ -0,0 +1,12 @@
+package chapter2page93;
+
+class exercise5 {
+    int inti;
+    double dubld;
+    boolean boolb;
+
+    public static void main(String[] args) {
+        exercise5 newExe = new exercise5();
+
+    }
+}
