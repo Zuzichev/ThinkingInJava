2611f015080258e46bd35a3f526a3c2144fe04d0
Zuzichev <zuzichev@mail.ru>
2021-06-20 14:18:28 +0500

    init

---
+++ src/chapter2page93/exercise4.java
@@ -0,0 +1,15 @@
+package chapter2page93;
+
+public class exercise4 {
+    static class DataOnly{
+        int i = 47;
+        double d = 1.2;
+        boolean b = false;
+    }
+
+    public static void main(String[] args) {
+        DataOnly asd = new DataOnly();
+        System.out.println("hi, DataOnly: ");
+        System.out.println(asd);
+    }
+}
