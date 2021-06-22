9dc7b2407876670de60c34bf78a81a49d3176242
Zuzichev <zuzichev@mail.ru>
2021-06-22 23:24:21 +0500

    init

--- src/chapter2page93/exercise6.java
+++ src/chapter2page93/exercise6.java
@@ -2,12 +2,11 @@ package chapter2page93;
 import java.util.*;
 
 public class exercise6 {
-    
-	public static void main(String[] args) {
-        String s = new String("12364");
-		
-		int storage(String s) {
-			return s.length()*2;
+	String s = new String("12364");
+	int storage(String s) {
+		return s.length()*2;
 		}
+	public static void main(String[] args) {
+        		
 		}
 }
