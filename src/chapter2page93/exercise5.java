0e4f1343dd6e026120562cc5f87cb1a8bb84c601
Zuzichev <zuzichev@mail.ru>
2021-06-22 23:19:17 +0500

    init

--- src/chapter2page93/exercise5.java
+++ src/chapter2page93/exercise5.java
@@ -1,12 +1,14 @@
 package chapter2page93;
 
-class exercise5 {
-    int inti;
-    double dubld;
-    boolean boolb;
+public class exercise5 {
+    static int inti = 47;
+    static double dubld = 1.1;
+    static boolean boolb = false;
 
     public static void main(String[] args) {
         exercise5 newExe = new exercise5();
-
+        System.out.println(inti);
+        System.out.println(dubld);
+        System.out.println(boolb);
     }
 }
---
+++ src/chapter2page93/exercise6.java
@@ -0,0 +1,13 @@
+package chapter2page93;
+import java.util.*;
+
+public class exercise6 {
+    
+	public static void main(String[] args) {
+        String s = new String("12364");
+		
+		int storage(String s) {
+			return s.length()*2;
+		}
+		}
+}
