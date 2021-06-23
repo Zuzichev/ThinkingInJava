8381bd30286fe8b070463ccb32479506639edd7c
Zuzichev <zuzichev@mail.ru>
2021-06-23 23:45:57 +0500

    init

---
+++ src/chapter2page93/exercise7.java
@@ -0,0 +1,14 @@
+package chapter2page93;
+
+public class exercise7 {
+
+    static int i = 47;
+    static void incremMy() { i++; }
+
+    public static void main(String[] args) {
+        exercise7 sf = new exercise7();
+        sf.incremMy();
+        System.out.println(sf);
+
+    }
+}
---
+++ src/chapter2page93/exercise8.java
@@ -0,0 +1,16 @@
+package chapter2page93;
+
+public class exercise8 {
+    static int isf = 21;
+
+    public static void main(String[] args) {
+        exercise8 exr81 = new exercise8();
+        exercise8 exr82 = new exercise8();
+        exercise8 exr83 = new exercise8();
+
+        System.out.println(exr81);
+        System.out.println(exr82);
+        System.out.println(exr83);
+        System.out.println(isf);
+    }
+}
