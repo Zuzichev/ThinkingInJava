58d049b1499d9ab93316eb18e205c25749906a3d
Zuzichev <zuzichev@mail.ru>
2021-06-26 20:06:04 +0500

    init

---
+++ src/chapter3page95/Exercise1.java
@@ -0,0 +1,14 @@
+package chapter3page95;
+
+import java.util.*;
+
+//импортировать статическую утилиту от Эккеля не получилось
+//выдает кучу ошибок и все, не понимаю почему
+//дочитаю до главы 6, там вроде должно нормально объясняться
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		System.out.println("Привет, сегодня");
+		System.out.println(new Date());
+	}
+}
---
+++ src/chapter3page95/Exercise2ch3.java
@@ -0,0 +1,23 @@
+package chapter3page95;
+
+class Tank {
+	float calibr;
+}
+
+public class Exercise2ch3 {
+	public static void main(String[] args) {
+		Tank t1 = new Tank();
+		Tank t2 = new Tank();
+		
+		t1.calibr = 1.19e-10f;
+		t2.calibr = 1.39e-10f;
+		
+		System.out.println("1: t1.calibr: " + t1.calibr + ", t2.calibr: " + t2.calibr);
+		
+		t1 = t2;
+		System.out.println("1: t1.calibr: " + t1.calibr + ", t2.calibr: " + t2.calibr);
+		
+		t1.calibr = 1.11e-10f;
+		System.out.println("1: t1.calibr: " + t1.calibr + ", t2.calibr: " + t2.calibr);
+	}
+}
