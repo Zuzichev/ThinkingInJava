789e090022554693030d1c273e2483ea0d782b3a
Zuzichev <zuzichev@mail.ru>
2021-06-27 00:43:39 +0500

    init

---
+++ src/chapter3page95/Exercise4ch3.java
@@ -0,0 +1,11 @@
+package chapter3page95;
+
+public class Exercise4ch3 {
+
+    static int r1 = 28, r0 = 1, t1 = 13, t0 = 3;
+    static double v;
+    public static void main(String[] args) {
+        v = (r1-r0)/(t1-t0);
+        System.out.println(v);
+    }
+}
