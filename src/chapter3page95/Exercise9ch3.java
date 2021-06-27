d2b4c7496cd87fee47f573562644b70f7e9c5c92
Zuzichev <zuzichev@mail.ru>
2021-06-27 22:23:21 +0500

    init

---
+++ src/chapter3page95/Exercise9ch3.java
@@ -0,0 +1,11 @@
+package chapter3page95;
+
+public class Exercise9ch3 {
+    public static void main(String[] args) {
+        double d1 = Double.MAX_VALUE;
+        float f1 = Float.MAX_VALUE;
+
+        System.out.println(d1);
+        System.out.println(f1);
+    }
+}
