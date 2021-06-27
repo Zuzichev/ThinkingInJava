ff5e3b7a819194a5970eac6bd902a452a9ba3726
Zuzichev <zuzichev@mail.ru>
2021-06-27 21:41:04 +0500

    init

---
+++ src/chapter3page95/Exercise8ch3.java
@@ -0,0 +1,14 @@
+package chapter3page95;
+
+public class Exercise8ch3 {
+    public static void main(String[] args) {
+        long l1 = 200L;
+        System.out.println("l1: " + Long.toBinaryString(l1));
+
+        long l2 = 0310;
+        System.out.println("l2: " + Long.toBinaryString(l2));
+
+        long l3 = 0xC8;
+        System.out.println("l3: " + Long.toBinaryString(l3));
+    }
+}
