ee147157aea62806c1185cbf88ea9928b85e44fe
Zuzichev <zuzichev@mail.ru>
2021-06-30 23:49:05 +0500

    init

--- src/chapter3page95/Exercise14ch3.java
+++ src/chapter3page95/Exercise14ch3.java
@@ -2,15 +2,13 @@ package chapter3page95;
 
 public class Exercise14ch3 {
     
-	class StringTest {
-		String x,y;
-		xnax = (x * y);
-	}
-	
 	public static void main(String[] args) {
-      StringTest abc = new StringTest("a","b");
-	  
-	  System.out.println(xnax.abc);
+        String s1 = "qwe";
+        String s2 = "rty";
+
+	    System.out.println(s1==s2);
+		System.out.println(s1!=s2);
+		System.out.println(s1.equals(s2));
 
         }
     }
