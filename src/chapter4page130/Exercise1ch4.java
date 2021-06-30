29d6546e5cbe5f15d263bab68a72ccc74056ad59
Zuzichev <zuzichev@mail.ru>
2021-07-01 00:07:34 +0500

    init

---
+++ src/chapter4page130/Exercise1ch4.java
@@ -0,0 +1,12 @@
+package chapter4page130;
+
+public class Exercise1ch4 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            if (i==100)
+                System.out.println(i + ".");
+            else
+                System.out.print(i + ", ");
+        }
+    }
+}
---
+++ src/chapter4page130/Exercise2ch4.java
@@ -0,0 +1,9 @@
+package chapter4page130;
+
+public class Exercise2ch4 {
+    public static void main(String[] args) {
+        for(int i=1; i<26; i++){
+
+        }
+    }
+}
