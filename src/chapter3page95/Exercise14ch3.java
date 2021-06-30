59a7bca4c705f3999a8c6229df3219285d609be2
Zuzichev <zuzichev@mail.ru>
2021-06-30 20:06:39 +0500

    init

--- src/chapter3page95/Exercise14ch3.java
+++ src/chapter3page95/Exercise14ch3.java
@@ -1,59 +1,17 @@
 package chapter3page95;
 
 public class Exercise14ch3 {
-    public static void main(String[] args) {
-        void f(boolean b) {}
-        String x = 'a';
-        String y = 'b';
-        void stringTest(String x, String у) {
-// Арифметические операции:
-            x = (String)(x * y);
-            x = (String)(x / у);
-            x = (String)(x % y);
-            x = (String)(x + y);
-            x = (String)(x - y);
-            X++J;
-            x--j;
-            x = (String)+ y;
-            x = (String) - y ;
-// Операции сравнения и логические операции:
-            f ( x > у );
-            f ( x >= у );
-            f ( x < у );
-            f ( x <= y );
-            f ( x == у );
-            f ( x != у );
-//! f ( ! x ) ;
-//1 f ( x && у ) ;
-// ! f ( x  || у ) ;
-/** Поразрядные операции:
-            x= (cha r)~ y;
-            x = ( c h a r ) (x  & у ) ;
-            x = ( c h a r ) (x | у ) ;
-            x = ( c h a r ) (x Л у ) ;
-            x = ( c h a r ) ( x << 1);
-            x = ( c h a r ) ( x >> 1);
-            x = ( c h a r ) ( x >>> 1 );
-// Совмещенное присваивание:
-            x +=  у;
-            x -= у;
-            x *= у;
-            x /= у;
-            x %= у;
-            x <<= 1;
-            x >>= 1;
-            x >>>= 1;
-            x &= у;
-            x ^= у;
-            x |= у;
-// Приведение:
-//!  boolean  b  = ( b o o le a n )x ;
-            b y te В = ( b y t e ) x ;
-            s h o r t s = ( s h o r t ) x ;
-            i n t i = ( i n t ) x ;
-            lo n g 1 = ( lo n g ) x ;
-            f l o a t f = ( f l o a t ) x ;
-            d ou ble d = (d o u b le ) x ;
-*/
+    
+	class StringTest {
+		String x,y;
+		xnax = (x * y);
+	}
+	
+	public static void main(String[] args) {
+      StringTest abc = new StringTest("a","b");
+	  
+	  System.out.println(xnax.abc);
+
+        }
     }
-}
+
